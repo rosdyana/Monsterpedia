4c5ef22f49026d5fd07c2b4addcc1e5a27c6dee1
rosdyana <rosdyana.kusuma@gmail.com>
2018-11-09 11:17:28 +0800

    [+] Add initial mvc project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Monsterpedia.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
